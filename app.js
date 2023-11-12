941fa69491ed821942051f2a86c3917ec1a66d7d
Alina Vizireanu <vizireanualina15@yahoo.com>
2023-11-12 19:03:32 +0200

    implement image resizing on the image from article

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
 const navList = document.querySelector(".navList");
-const body = document.querySelector("body");
+const articleImg = document.querySelector(".img_article");
+
+document.addEventListener("DOMContentLoaded", () => {
+  function updateImageSrc() {
+    let windowWidth = window.innerWidth;
+
+    if (windowWidth <= 600) {
+      articleImg.src = "./assets/images/image-web-3-mobile.jpg";
+    } else {
+      articleImg.src = "./assets/images/image-web-3-desktop.jpg";
+    }
+  }
+  // Initial call to set the image source on page load
+  updateImageSrc();
+  // Update image source on window resize
+  window.addEventListener("resize", updateImageSrc);
+});
 
 hamburgerMenu.addEventListener("click", () => {
   navList.classList.toggle("visible");
-  body.classList.toggle("overlay");
+  document.body.classList.toggle("overlay");
   if (navList.classList.contains("visible")) {
     hamburgerMenu.src = "./assets/images/icon-menu-close.svg";
   } else {
