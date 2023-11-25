e56a158662bff81055a006da2e1e838399c8b992
Alina Vizireanu <vizireanualina15@yahoo.com>
2023-11-25 18:19:11 +0200

    implement the header part for desktop

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
   function updateImageSrc() {
     let windowWidth = window.innerWidth;
 
-    if (windowWidth <= 600) {
+    if (windowWidth < 600) {
       articleImg.src = "./assets/images/image-web-3-mobile.jpg";
     } else {
       articleImg.src = "./assets/images/image-web-3-desktop.jpg";
+      hamburgerMenu.src = "./assets/images/icon-menu.svg";
+      hamburgerMenu.alt = "three horizontal lines";
+
+      if (navList.classList.contains("visible")) {
+        navList.classList.remove("visible");
+      }
+      if (document.body.classList.contains("overlay")) {
+        document.body.classList.remove("overlay");
+      }
     }
   }
   // Initial call to set the image source on page load
@@ -21,9 +30,12 @@ document.addEventListener("DOMContentLoaded", () => {
 hamburgerMenu.addEventListener("click", () => {
   navList.classList.toggle("visible");
   document.body.classList.toggle("overlay");
+
   if (navList.classList.contains("visible")) {
     hamburgerMenu.src = "./assets/images/icon-menu-close.svg";
+    hamburgerMenu.alt = "two lines placed in x position";
   } else {
     hamburgerMenu.src = "./assets/images/icon-menu.svg";
+    hamburgerMenu.alt = "three horizontal lines";
   }
 });
